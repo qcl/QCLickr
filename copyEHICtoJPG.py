5bfb7e0ddb5f9e6529ff6a1c0bc6c131ff28cd47
Qing-Cheng Li <qc.linux@gmail.com>
2018-05-16 20:55:56 +0200

    use exiftool to copy then attach exif info on converted file

--- copyEHICtoJPG.py
+++ copyEHICtoJPG.py
@@ -14,36 +14,42 @@ def convert(sourceDir, targetDir):
     # TODO if target path didn't exist, create one.
 
     files = os.listdir(sourceDir)
+    files.sort()
+    print('there is %d files in source dir' % (len(files)))
+    files = list(filter(lambda x: len(x) > 5 and x[-5:] in ['.heic', 'HEIC'], files))
+    print('and %d files are HEIC file' % (len(files)))
+    totalHEIC = len(files)
+    count = 1
     for filename in files:
+        print('[ %d / %d ]' % (count, totalHEIC))
+        count += 1
         
         filepath = os.path.join(sourceDir, filename)
         filename, ext = os.path.splitext(filename)
 
-        if ext not in ['.heic', '.HEIC']:
-            # skip
-            continue
-
         #print(filename)
-        targetPath = os.path.join(targetDir, '%s.%s' % (filename, 'png'))
+        targetPath = os.path.join(targetDir, '%s.%s' % (filename, 'jpg'))
 
         if os.path.exists(targetPath):
             print('%s is already there' % (targetPath))
             continue
 
-        convertHeicToPng(filepath, targetPath)
+        convertHeicToJpg(filepath, targetPath)
+        break
 
 
-def convertHeicToPng(sourcePath, targetPath):
+def convertHeicToJpg(sourcePath, targetPath):
     print('convert %s to %s' % (sourcePath, targetPath))
     #FIXME for now, it won't bring EXIF info from heic to png
-    #subprocess.run(['convert', sourcePath, targetPath])
+    subprocess.run(['convert', sourcePath, targetPath])
+    subprocess.run(['exiftool', '-TagsFromFile', sourcePath, targetPath, '-overwrite_original'])
 
 if __name__ == '__main__':
-    print('Convert .heic to .png in a dir')
+    print('Convert .heic to .jpg in a dir')
 
     if len(sys.argv) > 2:
         convert(sys.argv[1], sys.argv[2])
     else:
-        print('Usage: $ python3 ch2p.py [dir_contains_heic] [target_result_dir]')
+        print('Usage: $ python3 copyHEICtoJPG.py [dir_contains_heic] [target_result_dir]')
 
 
